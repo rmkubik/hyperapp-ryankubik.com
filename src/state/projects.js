7ff32c194b9fcf982b41ec3defe259ad6e567394
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-09-11 14:29:17 -0700

    Add links to all projects and image for eclipse

--- src/state/projects.js
+++ src/state/projects.js
@@ -3,8 +3,9 @@ export default {
     title: 'Eclipse',
     thumbnail: {
       blurb: 'Blurb about Eclipse',
-      src: 'https://via.placeholder.com/300x400',
+      src: 'assets/images/projects/eclipse.png',
     },
+    link: 'https://law5guy.itch.io/eclipse',
   },
   trainsmission: {
     title: 'Trainsmission',
@@ -12,6 +13,7 @@ export default {
       src: 'assets/images/projects/trainsmission.png',
       blurb: 'Can you get to the bottom of the murder mystery?',
     },
+    link: 'https://law5guy.itch.io/trainsmission',
   },
   unstable: {
     title: 'Unstable',
@@ -19,6 +21,7 @@ export default {
       src: 'assets/images/projects/unstable_pre_alpha_2.png',
       blurb: 'Adventure through a series of different worlds to find your way back home.',
     },
+    link: 'https://unstablegame.com',
   },
   infiniteIcons: {
     title: 'Infinite Icons',
@@ -26,6 +29,7 @@ export default {
       src: 'assets/images/projects/infinite_icons.png',
       blurb: 'Your desktop is getting out of control!',
     },
+    link: 'https://ldjam.com/events/ludum-dare/40/infinite-desktop-icons',
   },
   hikeintheWoods: {
     title: 'A Hike in the Woods',
@@ -33,6 +37,7 @@ export default {
       src: 'assets/images/projects/hike_in_the_woods.gif',
       blurb: 'Wander around in the woods to try and get back to your tent.',
     },
+    link: 'http://js13kgames.com/entries/a-hike-in-the-woods',
   },
   socialNetwork: {
     title: 'A Social Network',
@@ -40,6 +45,7 @@ export default {
       src: 'assets/images/projects/social_network.png',
       blurb: 'A little social network simulation. Connect all of the computers and people!',
     },
+    link: 'https://ldjam.com/events/ludum-dare/38/a-social-network',
   },
   lobsterbotomy: {
     title: 'Lobsterbotomy',
@@ -47,6 +53,7 @@ export default {
       src: 'assets/images/projects/lobsterbotomy.gif',
       blurb: 'Can you find love controlling your human as you get back in the dating game?',
     },
+    link: 'https://wvugamedevelopers.itch.io/lobsterbotomy',
   },
   sweetSpooky: {
     title: 'Sweet & Spooky',
@@ -54,6 +61,7 @@ export default {
       src: 'assets/images/projects/sweet_n_spooky.gif',
       blurb: 'Explore as far as you can into this spooky mansion in this fast paced shooter.',
     },
+    link: 'http://www.gm48.net/game/?id=331',
   },
   voxelTerrain: {
     title: 'Procedural Voxel Terrain Generator',
@@ -61,6 +69,7 @@ export default {
       src: 'assets/images/projects/terrain_gen.png',
       blurb: 'Fly around procedurally generated terrain inside a custom built voxel engine.',
     },
+    link: '',
   },
   matchEmAll: {
     title: "Gotta Match 'Em All",
@@ -68,6 +77,7 @@ export default {
       src: 'assets/images/projects/matchem.png',
       blurb: 'Match all the pokemon copies together in this Pokemon inspired concentration game.',
     },
+    link: 'https://ryankubik.com/matchem/matchem.php',
   },
   aStarDemo: {
     title: 'A* Pathfinding Demo',
@@ -75,6 +85,7 @@ export default {
       src: 'assets/images/projects/pathfinder.png',
       blurb: 'Place piles of gold and flood tiles to entice adventurers to move around the map.',
     },
+    link: 'https://law5guy.itch.io/astar-pathfinder-demo',
   },
   trainsmissionProductPage: {
     title: 'Trainsmission Product Page',
@@ -82,6 +93,7 @@ export default {
       src: 'assets/images/projects/trainsmission-site.png',
       blurb: 'Promotional site for Global Game Jam 2018 game, Trainsmission',
     },
+    link: 'http://www.trainsmission.com',
   },
   editEveryDamnDay: {
     title: 'Edit Every Damn Day Calendar',
@@ -89,6 +101,7 @@ export default {
       src: 'assets/images/projects/calendar.png',
       blurb: '30 seconds of video edited every day by @AldenRoth2 on Instagram',
     },
+    link: 'https://github.com/rmkubik/eedd-calendar',
   },
   robotsVsAliens: {
     title: 'Robots vs. Aliens Twitter Bot',
@@ -96,5 +109,6 @@ export default {
       src: 'assets/images/projects/robovsalien.png',
       blurb: 'A simulation of robots fighting aliens hourly',
     },
+    link: 'https://github.com/rmkubik/robovsalien',
   },
 };
