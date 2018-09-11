7fddcd4511237fa2b85c20614618d16580d2ba7c
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-09-11 15:07:05 -0700

    Correctly import images

--- src/state/projects.js
+++ src/state/projects.js
@@ -1,16 +1,31 @@
+import eclipseThumbnailImage from '../../assets/images/projects/eclipse.png';
+import trainsmissionThumbnailImage from '../../assets/images/projects/trainsmission.png';
+import unstableThumbnailImage from '../../assets/images/projects/unstable_pre_alpha_2.png';
+import infiniteIconsThumbnailImage from '../../assets/images/projects/infinite_icons.png';
+import socialNetworkThumbnailImage from '../../assets/images/projects/social_network.png';
+import hikeintheWoodsThumbnailImage from '../../assets/images/projects/hike_in_the_woods.gif';
+import lobsterbotomyThumbnailImage from '../../assets/images/projects/lobsterbotomy.gif';
+import sweetSpookyThumbnailImage from '../../assets/images/projects/sweet_n_spooky.gif';
+import voxelTerrainThumbnailImage from '../../assets/images/projects/terrain_gen.png';
+import matcheEmAllThumbnailImage from '../../assets/images/projects/matchem.png';
+import aStarDemoThumbnailImage from '../../assets/images/projects/pathfinder.png';
+import trainsmissionProductPageThumbnailImage from '../../assets/images/projects/trainsmission-site.png';
+import editEveryDamnDayThumbnailImage from '../../assets/images/projects/calendar.png';
+import robotsVsAliensThumbnailImage from '../../assets/images/projects/robovsalien.png';
+
 export default {
   eclipse: {
     title: 'Eclipse',
     thumbnail: {
       blurb: 'Enjoy the view!',
-      src: 'assets/images/projects/eclipse.png',
+      src: eclipseThumbnailImage,
     },
     link: 'https://law5guy.itch.io/eclipse',
   },
   trainsmission: {
     title: 'Trainsmission',
     thumbnail: {
-      src: 'assets/images/projects/trainsmission.png',
+      src: trainsmissionThumbnailImage,
       blurb: 'Can you get to the bottom of the murder mystery?',
     },
     link: 'https://law5guy.itch.io/trainsmission',
@@ -18,7 +33,7 @@ export default {
   unstable: {
     title: 'Unstable',
     thumbnail: {
-      src: 'assets/images/projects/unstable_pre_alpha_2.png',
+      src: unstableThumbnailImage,
       blurb: 'Adventure through a series of different worlds to find your way back home.',
     },
     link: 'https://unstablegame.com',
@@ -26,7 +41,7 @@ export default {
   infiniteIcons: {
     title: 'Infinite Icons',
     thumbnail: {
-      src: 'assets/images/projects/infinite_icons.png',
+      src: infiniteIconsThumbnailImage,
       blurb: 'Your desktop is getting out of control!',
     },
     link: 'https://ldjam.com/events/ludum-dare/40/infinite-desktop-icons',
@@ -34,7 +49,7 @@ export default {
   hikeintheWoods: {
     title: 'A Hike in the Woods',
     thumbnail: {
-      src: 'assets/images/projects/hike_in_the_woods.gif',
+      src: hikeintheWoodsThumbnailImage,
       blurb: 'Wander around in the woods to try and get back to your tent.',
     },
     link: 'http://js13kgames.com/entries/a-hike-in-the-woods',
@@ -42,7 +57,7 @@ export default {
   socialNetwork: {
     title: 'A Social Network',
     thumbnail: {
-      src: 'assets/images/projects/social_network.png',
+      src: socialNetworkThumbnailImage,
       blurb: 'A little social network simulation. Connect all of the computers and people!',
     },
     link: 'https://ldjam.com/events/ludum-dare/38/a-social-network',
@@ -50,7 +65,7 @@ export default {
   lobsterbotomy: {
     title: 'Lobsterbotomy',
     thumbnail: {
-      src: 'assets/images/projects/lobsterbotomy.gif',
+      src: lobsterbotomyThumbnailImage,
       blurb: 'Can you find love controlling your human as you get back in the dating game?',
     },
     link: 'https://wvugamedevelopers.itch.io/lobsterbotomy',
@@ -58,7 +73,7 @@ export default {
   sweetSpooky: {
     title: 'Sweet & Spooky',
     thumbnail: {
-      src: 'assets/images/projects/sweet_n_spooky.gif',
+      src: sweetSpookyThumbnailImage,
       blurb: 'Explore as far as you can into this spooky mansion in this fast paced shooter.',
     },
     link: 'http://www.gm48.net/game/?id=331',
@@ -66,7 +81,7 @@ export default {
   voxelTerrain: {
     title: 'Procedural Voxel Terrain Generator',
     thumbnail: {
-      src: 'assets/images/projects/terrain_gen.png',
+      src: voxelTerrainThumbnailImage,
       blurb: 'Fly around procedurally generated terrain inside a custom built voxel engine.',
     },
     link: '',
@@ -74,7 +89,7 @@ export default {
   matchEmAll: {
     title: "Gotta Match 'Em All",
     thumbnail: {
-      src: 'assets/images/projects/matchem.png',
+      src: matcheEmAllThumbnailImage,
       blurb: 'Match all the pokemon copies together in this Pokemon inspired concentration game.',
     },
     link: 'https://ryankubik.com/matchem/matchem.php',
@@ -82,7 +97,7 @@ export default {
   aStarDemo: {
     title: 'A* Pathfinding Demo',
     thumbnail: {
-      src: 'assets/images/projects/pathfinder.png',
+      src: aStarDemoThumbnailImage,
       blurb: 'Place piles of gold and flood tiles to entice adventurers to move around the map.',
     },
     link: 'https://law5guy.itch.io/astar-pathfinder-demo',
@@ -90,15 +105,15 @@ export default {
   trainsmissionProductPage: {
     title: 'Trainsmission Product Page',
     thumbnail: {
-      src: 'assets/images/projects/trainsmission-site.png',
-      blurb: 'Promotional site for Global Game Jam 2018 game, Trainsmission',
+      src: trainsmissionProductPageThumbnailImage,
+      blurb: 'Promotional site for Global Game Jam 2018 game, Trainsmission.',
     },
     link: 'http://www.trainsmission.com',
   },
   editEveryDamnDay: {
     title: 'Edit Every Damn Day Calendar',
     thumbnail: {
-      src: 'assets/images/projects/calendar.png',
+      src: editEveryDamnDayThumbnailImage,
       blurb: '30 seconds of video edited every day by @AldenRoth2 on Instagram',
     },
     link: 'https://github.com/rmkubik/eedd-calendar',
@@ -106,7 +121,7 @@ export default {
   robotsVsAliens: {
     title: 'Robots vs. Aliens Twitter Bot',
     thumbnail: {
-      src: 'assets/images/projects/robovsalien.png',
+      src: robotsVsAliensThumbnailImage,
       blurb: 'A simulation of robots fighting aliens hourly',
     },
     link: 'https://github.com/rmkubik/robovsalien',
