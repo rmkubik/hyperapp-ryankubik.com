c79b88a3c2d5f1e5b0bdced95b268118b1621bcb
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-09-11 11:07:51 -0700

    Create nav state

---
+++ src/state/nav.js
@@ -0,0 +1,24 @@
+export default {
+  title: {
+    text: 'Ryan Kubik',
+    link: '/',
+  },
+  links: [
+    {
+      text: 'Writing',
+      link: '/',
+    },
+    {
+      text: 'Code',
+      link: '/',
+    },
+    {
+      text: 'Contact',
+      link: '/',
+    },
+    {
+      text: 'Mailing List',
+      link: '/',
+    },
+  ],
+};
