a2fa767252225c82330adf950bb5eba90a172d49
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-09-11 15:17:35 -0700

    Import link icons in state

--- src/state/nav.js
+++ src/state/nav.js
@@ -1,3 +1,8 @@
+import writingIconImage from '../../assets/images/icons/painter.svg';
+import codeIconImage from '../../assets/images/icons/computer.svg';
+import contactIconImage from '../../assets/images/icons/twitter.svg';
+import mailingListIconImage from '../../assets/images/icons/linkedin.svg';
+
 export default {
   title: {
     text: 'Ryan Kubik',
@@ -7,18 +12,22 @@ export default {
     {
       text: 'Writing',
       link: '/',
+      icon: writingIconImage,
     },
     {
       text: 'Code',
       link: '/',
+      icon: codeIconImage,
     },
     {
       text: 'Contact',
       link: '/',
+      icon: contactIconImage,
     },
     {
       text: 'Mailing List',
       link: '/',
+      icon: mailingListIconImage,
     },
   ],
 };
