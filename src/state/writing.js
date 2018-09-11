fc8ce47e7871524fbe28154035477eaa97fa6326
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-09-11 15:08:44 -0700

    Correctly import writing images

--- src/state/writing.js
+++ src/state/writing.js
@@ -1,10 +1,13 @@
+import ld40devlogThumbnailImage from '../../assets/images/writing/ld40devlog.gif';
+import ld38postmortemThumbnailImage from '../../assets/images/writing/ld38postmortem.png';
+
 export default {
   ld40devlog: {
     title: 'LD 40 Devlog - Infinite Desktop Icons',
     thumbnail: {
       blurb:
         'The theme is “The More You Have, the Worse it Gets”! Of the other top themes, this was not one of the ones I was very excited about. I spent the most of Friday night brainstorming to try and come up with a game idea...',
-      src: 'assets/images/writing/ld40devlog.gif',
+      src: ld40devlogThumbnailImage,
     },
     link:
       'https://ldjam.com/events/ludum-dare/40/infinite-desktop-icons/ld-40-devlog-infinite-desktop-icons',
@@ -14,7 +17,7 @@ export default {
     thumbnail: {
       blurb:
         'Working alone is harder than working on a team. You really find out what your personal weak points are. I didn’t have much appreciation for developing skills outside of my wheelhouse. Its very easy to discard other skills when you’re working with talented people who can handle those departments, but a solo jam is a whole different animal...',
-      src: 'assets/images/writing/ld38postmortem.png',
+      src: ld38postmortemThumbnailImage,
     },
     link: 'https://ldjam.com/events/ludum-dare/38/a-social-network/ld38-postmortem',
   },
