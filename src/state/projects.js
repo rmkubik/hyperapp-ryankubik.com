9a08dde4a542ce09ee3d3ae5308fcc78c6094622
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-09-11 12:44:55 -0700

    Update projects from old site

--- src/state/projects.js
+++ src/state/projects.js
@@ -1,23 +1,100 @@
 export default {
-  unstable: {
-    title: 'Unstable',
-    thumbnail: {
-      src: 'https://via.placeholder.com/400x300',
-      blurb: 'Blurb about Unstable',
-    },
-  },
-  trainsmission: {
-    title: 'Trainsmission',
-    thumbnail: {
-      blurb: 'Can you get to the bottom of the murder mystery?',
-      src: 'https://via.placeholder.com/300',
-    },
-  },
   eclipse: {
     title: 'Eclipse',
     thumbnail: {
       blurb: 'Blurb about Eclipse',
       src: 'https://via.placeholder.com/300x400',
+    },
+  },
+  trainsmission: {
+    title: 'Trainsmission',
+    thumbnail: {
+      src: 'assets/images/projects/trainsmission.png',
+      blurb: 'Can you get to the bottom of the murder mystery?',
+    },
+  },
+  unstable: {
+    title: 'Unstable',
+    thumbnail: {
+      src: 'assets/images/projects/unstable_pre_alpha_2.png',
+      blurb: 'Adventure through a series of different worlds to find your way back home.',
+    },
+  },
+  infiniteIcons: {
+    title: 'Infinite Icons',
+    thumbnail: {
+      src: 'assets/images/projects/infinite_icons.png',
+      blurb: 'Your desktop is getting out of control!',
+    },
+  },
+  aHikeintheWoods: {
+    title: 'A Hike in the Woods',
+    thumbnail: {
+      src: 'assets/images/projects/hike_in_the_woods.gif',
+      blurb: 'Wander around in the woods to try and get back to your tent.',
+    },
+  },
+  aSocialNetwork: {
+    title: 'A Social Network',
+    thumbnail: {
+      src: 'assets/images/projects/social_network.png',
+      blurb: 'A little social network simulation. Connect all of the computers and people!',
+    },
+  },
+  lobsterbotomy: {
+    title: 'Lobsterbotomy',
+    thumbnail: {
+      src: 'assets/images/projects/lobsterbotomy.gif',
+      blurb: 'Can you find love controlling your human as you get back in the dating game?',
+    },
+  },
+  'sweet&Spooky': {
+    title: 'Sweet & Spooky',
+    thumbnail: {
+      src: 'assets/images/projects/sweet_n_spooky.gif',
+      blurb: 'Explore as far as you can into this spooky mansion in this fast paced shooter.',
+    },
+  },
+  proceduralVoxelTerrainGenerator: {
+    title: 'Procedural Voxel Terrain Generator',
+    thumbnail: {
+      src: 'assets/images/projects/terrain_gen.png',
+      blurb: 'Fly around procedurally generated terrain inside a custom built voxel engine.',
+    },
+  },
+  "gottaMatch'EmAll": {
+    title: "Gotta Match 'Em All",
+    thumbnail: {
+      src: 'assets/images/projects/matchem.png',
+      blurb: 'Match all the pokemon copies together in this Pokemon inspired concentration game.',
+    },
+  },
+  'a*PathfindingDemo': {
+    title: 'A* Pathfinding Demo',
+    thumbnail: {
+      src: 'assets/images/projects/pathfinder.png',
+      blurb: 'Place piles of gold and flood tiles to entice adventurers to move around the map.',
+    },
+  },
+  trainsmissionProductPage: {
+    title: 'Trainsmission Product Page',
+    thumbnail: {
+      src: 'assets/images/projects/trainsmission-site.png',
+      blurb: 'Promotional site for Global Game Jam 2018 game, Trainsmission',
+    },
+  },
+  editEveryDamnDayCalendar: {
+    title: 'Edit Every Damn Day Calendar',
+    thumbnail: {
+      src: 'assets/images/projects/calendar.png',
+      blurb: '30 seconds of video edited every day by @AldenRoth2 on Instagram',
+    },
+  },
+  'robotsvs.AliensTwitterBot': {
+    title: 'Robots vs. Aliens Twitter Bot',
+    thumbnail: {
+      src: 'assets/images/projects/robovsalien.png',
+      blurb: 'A simulation of robots fighting aliens hourly',
     },
   },
 };
