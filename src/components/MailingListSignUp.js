7f2a428ad43a519e3b352a0d46349467ac1f2161
Ryan Kubik <rmkubik@me.com>
2019-01-11 21:46:31 -0800

    Switch mailing list link from mail chimp to buttondown

--- src/components/MailingListSignUp.js
+++ src/components/MailingListSignUp.js
@@ -1,48 +1,28 @@
-import { h } from 'hyperapp';
+import { h } from "hyperapp";
 
 export default () => (
   <div>
     <h3>Mailing List</h3>
     <div id="mc_embed_signup">
       <form
-        action="https://ryankubik.us17.list-manage.com/subscribe/post?u=9b8c018898653292cbe3711f0&amp;id=57cdfbb895"
+        action="https://buttondown.email/api/emails/embed-subscribe/rmkubik"
         method="post"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
-        className="validate"
-        target="_blank"
-        novalidate
+        target="popupwindow"
+        onsubmit="window.open('https://buttondown.email/rmkubik', 'popupwindow')"
+        class="embeddable-buttondown-form"
       >
-        <div id="mc_embed_signup_scroll">
-          <input
-            type="email"
-            value=""
-            name="EMAIL"
-            className="email"
-            id="mce-EMAIL"
-            placeholder="email address"
-            required
-          />
-          {/* real people should not fill this in and expect good things
-          - do not remove this or risk form bot signups */}
-          <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
-            <input
-              type="text"
-              name="b_9b8c018898653292cbe3711f0_57cdfbb895"
-              tabindex="-1"
-              value=""
-            />
-          </div>
-          <div className="clear">
-            <input
-              type="submit"
-              value="Subscribe"
-              name="subscribe"
-              id="mc-embedded-subscribe"
-              className="button"
-            />
-          </div>
-        </div>
+        {/* <label for="bd-email" class="label">
+          Enter your email
+        </label> */}
+        <input
+          type="email"
+          name="email"
+          id="bd-email"
+          class="email"
+          placeholder="email address"
+        />
+        <input type="hidden" value="1" name="embed" />
+        <input type="submit" value="Subscribe" class="button" />
       </form>
       {"Sign up to hear updates on what I'm working on!"}
     </div>
